cdac3b026e0b260216837e167444ce63639ef567
Alan Maldonado <maldonadoalanivan@gmail.com>
2022-09-26 09:11:25 -0300

    Primer Bloque

---
+++ 01_var .playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+// Tipos de variables
+
+var edad = 20
+
+let nombre = "Alan Maldonado"
+
+print("Hola mundo! mi nombre es \(nombre) y tengo \(edad) años")
+
+var numeroFloat : Float = 21.02
+
+var numeroDouble : Double = 21.567523123
+
+let letra : Character = "A"
+
+var verdad : Bool = true
+
+var falso : Bool = false
+
+
+// Este es un comentario
+
+/*
+ Este es un comentario multilinea y todo lo que este dentro va a ser ignorado por el compilador.87
+ */
+
+
+// Casting
+
+let resultado = numeroFloat + Float(numeroDouble)
